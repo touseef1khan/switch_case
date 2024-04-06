6fdf1aa68e5f2175d92b8355a74272b2fabf2010
Touseef1khan <touseef332255@gmail.com>
2024-04-07 00:44:56 +0500

    great

---
+++ main.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//step 01
+const answers:{
+  numberOne: number,
+  numberTwo: number,
+  operator:string, 
+} = await inquirer.prompt([
+    {type: "number",
+     name: "numberOne",
+    message: "Enter your first number"
+  },
+    {type: "number",
+     name: "numberTwo",
+    message: "Enter your second number"
+  },
+  { type: "list",
+    name: "operator",
+  message: "Enter your operator",
+  choices:["+", "-", "*", "/"]}
+])
+
+// step02
+const { numberOne,numberTwo, operator } = answers;
+
+let result
+switch (operator) {
+  case "+" : result = numberOne + numberTwo;
+  break;
+  case "-" : result = numberOne - numberTwo;
+  break;
+  case "*" : result = numberOne * numberTwo;
+  break;
+  case "/" : result = numberOne / numberTwo;
+  break;
+  default:
+  console.log("invalid operator");
+}
+console.log(`${numberOne} ${operator} ${numberTwo} =${result}`);
